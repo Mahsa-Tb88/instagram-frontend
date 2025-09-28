33cd657c067ba804b342c9bcf48880e257518598
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-09-27 21:01:41 -0700

    add types

---
+++ src/types/global.d.ts
@@ -0,0 +1,14 @@
+import type { PaletteColorOptions } from "@mui/material";
+
+declare global {
+    var SERVER_URL: string;
+    var API_BASE_URL: string;
+}
+
+declare module "@mui/material" {
+    interface PaletteOptions {
+        light?: PaletteColorOptions;
+    }
+}
+
+export {};
---
+++ src/utils/globalconstants.ts
@@ -0,0 +1,2 @@
+globalThis.SERVER_URL = "http://localhost:3000";
+globalThis.API_BASE_URL = "http://localhost:3000/api/v1";
