253b4a9ac12fc0a395de9c46bcf3c6cace11e036
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-09-28 22:25:00 -0700

    add main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from "react-dom/client";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { RouterProvider } from "react-router";
+import router from "./router/router";
+import { ToastContainer } from "react-toastify";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      retry: false,
+      gcTime: 30000,
+      staleTime: 10000,
+    },
+  },
+});
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+    <ToastContainer theme="colored" position="bottom-center" />
+  </QueryClientProvider>
+);
