1693225dc4dd4e6d0ba46d266b88ffbd11e7a029
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-09-28 22:43:22 -0700

    add initializer

--- src/layouts/Initializer/Initializer.tsx
+++ src/layouts/Initializer/Initializer.tsx
@@ -1,5 +1,38 @@
-import React from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import React, { useEffect } from "react";
+import { useInitialize } from "../../http/queries";
+import { useAppStore, useUserStore } from "../../store/store";
+import { LinearProgress, Stack } from "@mui/material";
+import LoadingError from "../../components/LoadingError";
+import logo from "../../assets/images/logo.png";
 
 export default function Initializer() {
-  return <div>Initializer</div>;
+  const { isPending, data, error, refetch } = useInitialize();
+  const setInitialized = useAppStore((state) => state.setInitialized);
+  const setUser = useUserStore((state) => state.setUser);
+  const setSuggested = useUserStore((state) => state.setSuggestedUsers);
+
+  useEffect(() => {
+    if (data) {
+      setInitialized(true);
+      const { body } = data.data;
+      if (body.user?.username) {
+        setUser(body.user);
+        setSuggested(body.suggested);
+      }
+    }
+  }, [data]);
+
+  return (
+    <Stack sx={{ height: "100vh", justifyContent: "center", alignItems: "center" }}>
+      {isPending ? (
+        <Stack sx={{ width: 300, alignItems: "center" }}>
+          <img src={logo} width={100} style={{ marginBottom: 30 }} />
+          <LinearProgress color="warning" sx={{ width: 1 }} />
+        </Stack>
+      ) : (
+        error && <LoadingError message={error.message} handleAction={refetch} />
+      )}
+    </Stack>
+  );
 }
