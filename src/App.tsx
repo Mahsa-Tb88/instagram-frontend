ddba6e870debdfe6b874eec82010265de6677cb7
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-09-28 22:32:15 -0700

    add App

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import { useState } from "react";
+/* eslint-disable react-hooks/exhaustive-deps */
+import { ThemeProvider } from "@emotion/react";
+import { CssBaseline, useMediaQuery } from "@mui/material";
+import { useAppStore } from "./store/store";
+import { useEffect } from "react";
+import theme from "./theme/theme";
+import { Outlet } from "react-router";
+import Initializer from "./layouts/Initializer/Initializer";
 
 function App() {
-  return <div>App</div>;
+  const isInitialized = useAppStore((state) => state.initialized);
+  const setIsMobile = useAppStore((state) => state.setIsMobile);
+  const isMobile = useMediaQuery("(max-width:899px)");
+
+  useEffect(() => {
+    setIsMobile(isMobile);
+  }, [isMobile]);
+
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {isInitialized ? <Outlet /> : <Initializer />}
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ src/layouts/Initializer/Initializer.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Initializer() {
+  return <div>Initializer</div>;
+}
