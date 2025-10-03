9393873a91dd8e50d8b7a9cf27dc7ef68196d52b
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-10-03 10:40:02 -0700

    create MainLayout

--- src/layouts/Main/MainLayout.tsx
+++ src/layouts/Main/MainLayout.tsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import { Box } from "@mui/material";
+import React from "react";
+import { useAppStore } from "../../store/store";
+import { useMustLoggedIn } from "../../utils/customhooks";
+import BottomMenu from "./BottomMenu";
+import LeftMenu from "./LeftMenu";
+import { Outlet } from "react-router";
 
 export default function MainLayout() {
+  const isMobile = useAppStore((state) => state.isMobile);
+  useMustLoggedIn();
+
   return (
-    <div>MainLayout</div>
-  )
+    <Box>
+      {isMobile ? <BottomMenu /> : <LeftMenu />}
+      <Outlet />
+    </Box>
+  );
 }
