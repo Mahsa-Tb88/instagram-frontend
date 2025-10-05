1273ee1514a811309d8fcecaa336ab797a4eaea0
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-10-04 23:12:30 -0700

    add card details

--- src/pages/Home/Post.tsx
+++ src/pages/Home/Post.tsx
@@ -1,14 +1,34 @@
-import { Avatar, Button, Card, CardHeader } from "@mui/material";
+import {
+  Avatar,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  IconButton,
+  Typography,
+} from "@mui/material";
 import React, { useState } from "react";
 import type { Post } from "../../types/types";
 import { Link } from "react-router";
+import parse from "html-react-parser";
+import { useUserStore } from "../../store/store";
+import { MdComment, MdFavorite } from "react-icons/md";
 
 type PostProps = { post: Post };
 export default function Post({ post }: PostProps) {
   const [followChanged, setFollowChanged] = useState(false);
   console.log("post is", post);
+  const userId = useUserStore((state) => state._id);
+  const liked = post.likes.includes(userId);
+  const [likeChanged, setLikeChanged] = useState(false);
   function handleFollowUnfollow() {}
   const dateOfPost = new Date(post.createdAt);
+
+  function handleLikeUnlike() {}
+  function showViewPostDialog() {}
+
   return (
     <Card>
       <CardHeader
@@ -37,6 +57,22 @@ export default function Post({ post }: PostProps) {
         subheader={dateOfPost.toDateString() + " - " + dateOfPost.toISOString().substring(11, 16)}
         slotProps={{ title: { fontWeight: 600 }, subheader: { fontSize: "12px" } }}
       />
+      <CardMedia image={SERVER_URL + post.image} component={"img"} />
+      <CardContent>
+        <Typography variant="body2" component={"div"}>
+          {parse(post.caption)}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <IconButton onClick={handleLikeUnlike}>
+          {liked != likeChanged ? <MdFavorite color="red" /> : <MdFavorite />}
+        </IconButton>
+        <Typography>{post.likes.length + (!likeChanged ? 0 : liked ? -1 : 1)}</Typography>
+        <IconButton onClick={() => showViewPostDialog(post._id)}>
+          <MdComment />
+        </IconButton>
+        <Typography>{post.comments.length}</Typography>
+      </CardActions>
     </Card>
   );
 }
