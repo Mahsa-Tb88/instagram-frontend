2ee002bdcf5cbb31b32f1bf3e8979feb887b7e47
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-10-04 21:46:59 -0700

    post header

--- src/pages/Home/Post.tsx
+++ src/pages/Home/Post.tsx
@@ -1,5 +1,42 @@
-import React from "react";
+import { Avatar, Button, Card, CardHeader } from "@mui/material";
+import React, { useState } from "react";
+import type { Post } from "../../types/types";
+import { Link } from "react-router";
 
-export default function Post() {
-  return <div>Post</div>;
+type PostProps = { post: Post };
+export default function Post({ post }: PostProps) {
+  const [followChanged, setFollowChanged] = useState(false);
+  console.log("post is", post);
+  function handleFollowUnfollow() {}
+  const dateOfPost = new Date(post.createdAt);
+  return (
+    <Card>
+      <CardHeader
+        avatar={
+          <Avatar
+            src={SERVER_URL + post.user.profilePicture}
+            alt={post.user.username}
+            component={Link}
+            to={"/user/" + post.user.username}
+            sx={{ textDecoration: "none" }}
+          />
+        }
+        action={
+          <Button variant="text" sx={{ fontWeight: 600 }} onClick={handleFollowUnfollow}>
+            {followChanged ? "Follow" : "Unfollow"}
+          </Button>
+        }
+        title={
+          <Link
+            to={"/user/" + post.user.username}
+            style={{ textDecoration: "none", color: "inherit" }}
+          >
+            {post.user.username.substring(0, 1).toUpperCase() + post.user.username.substring(1)}
+          </Link>
+        }
+        subheader={dateOfPost.toDateString() + " - " + dateOfPost.toISOString().substring(11, 16)}
+        slotProps={{ title: { fontWeight: 600 }, subheader: { fontSize: "12px" } }}
+      />
+    </Card>
+  );
 }
