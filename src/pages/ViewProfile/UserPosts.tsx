865e1e8c231f6b56bee858c83c8fa6fe5dbb4682
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-10-25 22:56:17 -0700

    add skeleton

--- src/pages/ViewProfile/UserPosts.tsx
+++ src/pages/ViewProfile/UserPosts.tsx
@@ -1,13 +1,15 @@
-import { Grid } from "@mui/material";
+import { Box, Grid, Stack } from "@mui/material";
 import { useParams } from "react-router";
 import { useGetUserPosts } from "../../http/queries";
+import UserPostsSkeleton from "./skeletons/UserPostsSkeleton";
 
 export default function UserPosts() {
-  const username = useParams<{ username: string }>();
-
+  const { username } = useParams<{ username: string }>();
+  const { data, isPending, refetch, error } = useGetUserPosts(username!, 9);
+  console.log("data...", data);
   return (
-    <Grid container>
-      <Grid size={{ xs: 12, md: 3, lg: 4 }}></Grid>
-    </Grid>
+    <Stack>
+      <UserPostsSkeleton />{" "}
+    </Stack>
   );
 }
---
+++ src/pages/ViewProfile/skeletons/UserPostsSkeleton.tsx
@@ -0,0 +1,62 @@
+import { Grid, Skeleton, Stack } from "@mui/material";
+
+export default function UserPostsSkeleton() {
+  return (
+    <Grid container my={3} columnSpacing={{ xs: 2, sm: 3, lg: 4 }}>
+      <Grid size={{ xs: 12, md: 4 }} mb={3}>
+        <Skeleton
+          variant="rectangular"
+          width={"100%"}
+          height={200}
+          animation="wave"
+          sx={{ borderRadius: "4px" }}
+        />
+      </Grid>
+      <Grid size={{ xs: 12, md: 4 }} mb={3}>
+        <Skeleton
+          variant="rectangular"
+          width={"100%"}
+          height={200}
+          animation="wave"
+          sx={{ borderRadius: "4px" }}
+        />
+      </Grid>
+      <Grid size={{ xs: 12, md: 4 }} mb={3}>
+        <Skeleton
+          variant="rectangular"
+          width={"100%"}
+          height={200}
+          animation="wave"
+          sx={{ borderRadius: "4px" }}
+        />
+      </Grid>
+      <Grid size={{ xs: 12, md: 4 }} mb={3}>
+        <Skeleton
+          variant="rectangular"
+          width={"100%"}
+          height={200}
+          animation="wave"
+          sx={{ borderRadius: "4px" }}
+        />
+      </Grid>
+      <Grid size={{ xs: 12, md: 4 }} mb={3}>
+        <Skeleton
+          variant="rectangular"
+          width={"100%"}
+          height={200}
+          animation="wave"
+          sx={{ borderRadius: "4px" }}
+        />
+      </Grid>
+      <Grid size={{ xs: 12, md: 4 }} mb={3}>
+        <Skeleton
+          variant="rectangular"
+          width={"100%"}
+          height={200}
+          animation="wave"
+          sx={{ borderRadius: "4px" }}
+        />
+      </Grid>
+    </Grid>
+  );
+}
