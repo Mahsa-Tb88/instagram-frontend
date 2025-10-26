f28221fde30cdaa906827ac124fd8a6e44d296da
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-10-26 09:28:07 -0700

    add like & button btns

--- src/pages/ViewProfile/UserPost.tsx
+++ src/pages/ViewProfile/UserPost.tsx
@@ -1,20 +1,26 @@
-import { Box, IconButton, Paper, Stack } from "@mui/material";
-import React from "react";
+import { Box, IconButton, Paper, Stack, Typography } from "@mui/material";
+import React, { useState } from "react";
 import type { Post } from "../../types/types";
-import { MdEdit } from "react-icons/md";
+import { MdComment, MdDelete, MdEdit, MdFavorite } from "react-icons/md";
 import { useUserStore } from "../../store/store";
 
 type PostProps = { post: Post };
 export default function UserPost({ post }: PostProps) {
   const username = useUserStore((state) => state.username);
+  const userId = useUserStore((state) => state._id);
+  const liked = post.likes.includes(userId);
+  const [likeChanged, setLikeChanged] = useState(false);
 
   return (
     <Stack my={3}>
-      <Paper sx={{ p: 1, height: 300 }}>
+      <Paper sx={{ p: 1, height: 350 }}>
         {username == post.user.username && (
-          <Box sx={{ textAlign: "right" }}>
-            <IconButton>
-              <MdEdit size={16} />
+          <Box sx={{ textAlign: "right", mb: 1 }}>
+            <IconButton sx={{ m: 0, p: "5px" }} color="info">
+              <MdEdit size={14} />
+            </IconButton>
+            <IconButton sx={{ m: 0, p: "5px" }} color="error">
+              <MdDelete size={14} />
             </IconButton>
           </Box>
         )}
@@ -23,12 +29,32 @@ export default function UserPost({ post }: PostProps) {
           alt="post"
           style={{
             width: "100%",
-            height: "100%",
+
             objectFit: "contain",
             display: "block",
             paddingBottom: "20%",
           }}
         />
+        <Stack flexDirection={"row"}>
+          <Box display={"flex"} flexDirection={"row"} alignItems={"center"} mr={2}>
+            <IconButton>
+              {liked != likeChanged ? (
+                <MdFavorite color="red" size={20} />
+              ) : (
+                <MdFavorite size={20} />
+              )}
+            </IconButton>
+            <Typography fontSize={14}>
+              {post.likes.length + (!likeChanged ? 0 : liked ? -1 : 1)}
+            </Typography>
+          </Box>
+          <Box display={"flex"} flexDirection={"row"} alignItems={"center"} mr={3}>
+            <IconButton>
+              <MdComment size={20} />
+            </IconButton>
+            <Typography fontSize={14}>{post.comments.length}</Typography>
+          </Box>
+        </Stack>
       </Paper>
     </Stack>
   );
