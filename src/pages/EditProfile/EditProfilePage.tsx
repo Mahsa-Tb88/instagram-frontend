e2c888779148dcf65c93ef781e6fe3d72498dc25
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-11-03 14:10:23 -0800

    edit profile UI

--- src/pages/EditProfile/EditProfilePage.tsx
+++ src/pages/EditProfile/EditProfilePage.tsx
@@ -1,34 +1,87 @@
-import { Divider, Stack, TextField, Typography } from "@mui/material";
-import React from "react";
-import { useUserStore } from "../../store/store";
+import { Box, Button, IconButton, Stack, TextField, Typography } from "@mui/material";
 import { useGetProfile } from "../../http/queries";
 import { useNavigate, useParams } from "react-router";
 import LoadingError from "../../components/LoadingError";
 import SkeletonEditProfile from "./SkeletonEditProfile";
-import { MdHome, MdRefresh } from "react-icons/md";
+import { MdClose, MdDelete, MdHome } from "react-icons/md";
 
 export default function EditProfilePage() {
   const { username } = useParams<{ username: string }>();
   const navigate = useNavigate();
   const { data, isPending, error, refetch } = useGetProfile(username!);
   const user = data?.data?.body;
+  console.log("error", error);
   return (
-    <Stack width={"80%"} sx={{ mx: 2 }}>
-      {!isPending ? (
+    <Stack width={"80%"} sx={{ mx: 2, my: 6 }}>
+      {isPending ? (
         <SkeletonEditProfile />
       ) : error ? (
         <LoadingError
           message={error.message}
-          handleAction={error.status === 404 ? () => navigate("/") : refetch}
-          actionText={error.status === 404 ? "Back to home" : "Retry"}
-          actionIcon={error.status === 404 ? <MdHome /> : <MdRefresh />}
+          handleAction={
+            error.status === 404
+              ? () => navigate("/")
+              : error.status == 401
+              ? () => navigate("/auth/login")
+              : refetch
+          }
+          actionText={
+            error.status === 404 ? "Back to home" : error.status == 401 ? "Login" : "Retry"
+          }
+          actionIcon={error.status === 404 && <MdHome />}
         />
       ) : (
-        <Stack>
-          <Typography component="h3" variant="h3">
+        <Stack maxWidth={"60%"} gap={3}>
+          <Typography component="h3" variant="h3" mb={1}>
             Edit Profile
           </Typography>
-          <TextField value={user?.username} />
+          <Stack gap={4}>
+            <TextField value={user?.username} label="username" disabled />
+            <TextField value={user?.fullname} label="Fullname" />
+            <TextField value={user?.email} label="Email" />
+            <TextField label="Password" />
+            <TextField label="Confirm Password" />
+          </Stack>
+          <Stack flexDirection={"row"} alignItems={"center"}>
+            <Typography sx={{ mr: 3 }}>Profile Picture</Typography>
+            <Stack
+              sx={{
+                width: "120px",
+                height: "120px",
+                borderRadius: "50%",
+                borderColor: "red",
+                border: "2px solid rgba(255, 0, 0, 0.6)",
+                overflow: "hidden",
+                alignItems: "center",
+                justifyContent: "center",
+                position: "relative",
+              }}
+            >
+              <Box
+                component={"img"}
+                src={SERVER_URL + user?.profilePicture}
+                alt="profile image"
+                sx={{ objectFit: "contain", width: "100%", height: "100%" }}
+              />
+              <IconButton
+                sx={{
+                  position: "absolute",
+                  top: "50%",
+                  left: "50%",
+                  transform: "translate(-50%, -50%)",
+                  bgcolor: "rgba(255, 0, 0, 0.6)",
+                  color: "white",
+                  "&:hover": {
+                    bgcolor: "rgba(255, 0, 0, 0.8)",
+                  },
+                }}
+              >
+                <MdClose />
+              </IconButton>
+            </Stack>
+          </Stack>
+
+          <Button>Save Changes</Button>
         </Stack>
       )}
     </Stack>
