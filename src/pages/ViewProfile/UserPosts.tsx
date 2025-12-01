3f8900f4c9153f1e22c8d185fa6b0b5bee94b87e
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-11-30 21:40:53 -0800

    revise error

--- src/pages/ViewProfile/UserPosts.tsx
+++ src/pages/ViewProfile/UserPosts.tsx
@@ -1,19 +1,19 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import { Box, Grid, Stack, Typography } from "@mui/material";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { useGetUserPosts } from "../../http/queries";
 import UserPostsSkeleton from "./skeletons/UserPostsSkeleton";
 import { useInView } from "react-intersection-observer";
 import LoadingError from "../../components/LoadingError";
 import { useEffect } from "react";
 import UserPost from "./UserPost";
+import { MdHome } from "react-icons/md";
 
 export default function UserPosts() {
   const { username } = useParams<{ username: string }>();
-  const { data, error, fetchNextPage, hasNextPage, isFetching, refetch } = useGetUserPosts(
-    username!,
-    9
-  );
+  const navigate = useNavigate();
+  const { data, error, fetchNextPage, hasNextPage, isFetching, refetch, isPending } =
+    useGetUserPosts(username!, 9);
 
   const { ref, inView } = useInView({ rootMargin: "150px" });
   useEffect(() => {
@@ -25,31 +25,44 @@ export default function UserPosts() {
 
   return (
     <Stack my={4}>
-      {isFetching && <UserPostsSkeleton />}
+      {isPending ? (
+        <UserPostsSkeleton />
+      ) : error && error.status === 404 ? (
+        <LoadingError
+          message={error.message}
+          handleAction={() => navigate("/")}
+          actionText="Back to home"
+          actionIcon={<MdHome />}
+        />
+      ) : !data?.pages[0].data.body.count ? (
+        <Typography sx={{ textAlign: "center" }} fontWeight={600} fontSize={24} p={2}>
+          No Posts yet!
+        </Typography>
+      ) : (
+        <Box maxWidth={800} mx="auto">
+          <Grid container columnSpacing={{ xs: 2, sm: 3, lg: 4 }} mb={3}>
+            {data?.pages?.map((page) => {
+              return page.data.body.posts.map((p) => (
+                <Grid size={{ xs: 12, md: 6, lg: 4 }} key={p._id}>
+                  <UserPost post={p} />
+                </Grid>
+              ));
+            })}
+          </Grid>
 
-      <Box maxWidth={800} mx="auto">
-        <Grid container columnSpacing={{ xs: 2, sm: 3, lg: 4 }} mb={3}>
-          {data?.pages?.map((page) => {
-            return page.data.body.posts.map((p) => (
-              <Grid size={{ xs: 12, md: 6, lg: 4 }} key={p._id}>
-                <UserPost post={p} />
-              </Grid>
-            ));
-          })}
-        </Grid>
+          {!hasNextPage && !isFetching && !error && (
+            <Box textAlign="center" bgcolor="#eee" p={4}>
+              {postExists ? (
+                <Typography>No more posts</Typography>
+              ) : (
+                <Typography>No post exists</Typography>
+              )}
+            </Box>
+          )}
 
-        {!hasNextPage && !isFetching && !error && (
-          <Box textAlign="center" bgcolor="#eee" p={4}>
-            {postExists ? (
-              <Typography>No more posts</Typography>
-            ) : (
-              <Typography>No post exists</Typography>
-            )}
-          </Box>
-        )}
-
-        {error && !isFetching && <LoadingError message={error.message} handleAction={refetch} />}
-      </Box>
+          {error && !isFetching && <LoadingError message={error.message} handleAction={refetch} />}
+        </Box>
+      )}
       <div ref={ref} />
     </Stack>
   );
