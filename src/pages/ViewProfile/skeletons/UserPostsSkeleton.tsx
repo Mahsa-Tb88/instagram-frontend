d04b837ffb2743c6fa3d66b5e05f2d529302e070
Mahsa <50694999+Mah-tab88@users.noreply.github.com>
2025-11-30 21:25:02 -0800

    revise skeleton

--- src/pages/ViewProfile/skeletons/UserPostsSkeleton.tsx
+++ src/pages/ViewProfile/skeletons/UserPostsSkeleton.tsx
@@ -2,61 +2,17 @@ import { Grid, Skeleton } from "@mui/material";
 
 export default function UserPostsSkeleton() {
   return (
-    <Grid container my={3} columnSpacing={{ xs: 2, sm: 3, lg: 4 }}>
-      <Grid size={{ xs: 12, md: 4 }} mb={3}>
-        <Skeleton
-          variant="rectangular"
-          width={"100%"}
-          height={200}
-          animation="wave"
-          sx={{ borderRadius: "4px" }}
-        />
-      </Grid>
-      <Grid size={{ xs: 12, md: 4 }} mb={3}>
-        <Skeleton
-          variant="rectangular"
-          width={"100%"}
-          height={200}
-          animation="wave"
-          sx={{ borderRadius: "4px" }}
-        />
-      </Grid>
-      <Grid size={{ xs: 12, md: 4 }} mb={3}>
-        <Skeleton
-          variant="rectangular"
-          width={"100%"}
-          height={200}
-          animation="wave"
-          sx={{ borderRadius: "4px" }}
-        />
-      </Grid>
-      <Grid size={{ xs: 12, md: 4 }} mb={3}>
-        <Skeleton
-          variant="rectangular"
-          width={"100%"}
-          height={200}
-          animation="wave"
-          sx={{ borderRadius: "4px" }}
-        />
-      </Grid>
-      <Grid size={{ xs: 12, md: 4 }} mb={3}>
-        <Skeleton
-          variant="rectangular"
-          width={"100%"}
-          height={200}
-          animation="wave"
-          sx={{ borderRadius: "4px" }}
-        />
-      </Grid>
-      <Grid size={{ xs: 12, md: 4 }} mb={3}>
-        <Skeleton
-          variant="rectangular"
-          width={"100%"}
-          height={200}
-          animation="wave"
-          sx={{ borderRadius: "4px" }}
-        />
-      </Grid>
+    <Grid container my={3} spacing={2}>
+      {Array.from({ length: 9 }).map((_, index) => (
+        <Grid size={{ xs: 12, sm: 4 }} key={index}>
+          <Skeleton
+            variant="rounded"
+            animation="wave"
+            sx={{ bgcolor: "grey.300", aspectRatio: 1, height: "auto" }}
+          />
+          ;
+        </Grid>
+      ))}
     </Grid>
   );
 }
